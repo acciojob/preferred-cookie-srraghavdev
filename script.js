747b442ba3c768278ca3c71c451e1e3c99ba5bc1
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-07-26 00:59:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 //your JS code here. If required.
 let c= document.getElementsByTagName('form')[0]
 let d=document.getElementsByTagName('input')
+var r = document.querySelector(':root');
 c.addEventListener('submit',(event)=>{
 	event.preventDefault()
-	document.cookie=`fontsize=${input[0].value}px`
-	document.cookie=`color=${input[1].value}`
+	document.cookie=`fontsize=${d[0].value}px`
+	document.cookie=`color=${d[1].value}`
 })
+function apply(){
+  let cookie=document.cookie
+  let split=cookie.split(';')
+  let fontsize=(split[0].split('='))[1]
+  let color=(split[1].split('='))[1]
+  r.style.setProperty('--fontsize',fontsize);
+  r.style.setProperty('--fontcolor',color);
+}
