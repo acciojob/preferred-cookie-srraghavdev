a09fec79083664c095857222cac63d4893d03a05
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-07-26 00:43:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let c= document.getElementsByTagName('form')[0]
+let d=document.getElementsByTagName('input')
+c.addEventListener('submit',(event)=>{
+	event.preventDefault()
+	document.cookie=`fontsize=${input[0].value}px`
+	document.cookie=`color=${input[1].value}`
+})
